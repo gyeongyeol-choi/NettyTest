84387e459d39b519568e8124195d4d2d94ec606c
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2022-06-22 19:53:50 +0900

    Initial Commit

---
+++ src/main/java/SimpleNettyClient/ClientHandler.java
@@ -0,0 +1,18 @@
+package SimpleNettyClient;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class ClientHandler extends SimpleChannelInboundHandler {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+        System.out.println((String)msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/test/java/com/tmax/netty/SimplenettyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenettyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
